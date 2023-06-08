1fe8a5d01fb38eaa3a536fd48c7558caeec6e3dd
nomaprof <nomaprof@gmail.com>
2023-06-08 14:47:45 +0000

    if else loop functions

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        value = number % 10
+    else:
+        value = number % -10
+        value *= -1
+
+    print("{:d}",format(value), end='')
+    return (value)
