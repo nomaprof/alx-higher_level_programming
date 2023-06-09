b8d1bf59b6aee64fe26a156e9b1cc17394f2a253
nomaprof <nomaprof@gmail.com>
2023-06-09 02:26:33 +0000

    Import modules tasks

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    number = len(sys.argv) - 1
+    if number == 0:
+        print("0 arguments.")
+    elif number == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(number))
+    for m in range(number):
+        print("{}: {}".format(m +1, sys.argv[m + 1]))
