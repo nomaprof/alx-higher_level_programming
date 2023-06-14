fec12df19e1d0fd31f1b653a5813e59ad26461e6
nomaprof <nomaprof@gmail.com>
2023-06-14 10:13:30 +0000

    List and tuples

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    """a function to remove character from a string"""
-    modified_ans = ""
+    """Remove a character from a string"""
+    ans = ""
     for m in range(len(my_string)):
         if my_string[m] != 'c' and my_string[m] != 'C':
-            modified_ans += modified_answer[m]
-    return modified_ans
+            ans += my_string[m]
+    return ans
