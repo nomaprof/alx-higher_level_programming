ffe752ada7164f03f92aca7d8c5eb4620e1f5b22
nomaprof <nomaprof@gmail.com>
2023-06-15 04:51:23 +0000

    Sets and dictionaries

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string)
+def roman_to_int(roman_string):
+    """ converts a Roman numeral to an integer."""
     romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     if (roman_string is None) or (type(roman_string) is not str):
         return 0
 
-    answer = len(reoman_string)
-    value_ans = romans[roman_string[answer - 1]]
-    for i in range(number - 1, 0, -1):
+    answer = len(roman_string)
+    param = romans[roman_string[answer-1]]
+    for i in range(answer - 1, 0, -1):
         recent_ans = romans[roman_string[i]]
-        past_ans = romans[roman_string[i - 1]]
+        past_ans = romans[roman_string[i-1]]
 
         if past_ans >= recent_ans:
-            value_ans += past_ans
+            param += past_ans
         else:
-            value_ans -= past_ans
+            param -= past_ans
 
-    return value_ans
+    return param
