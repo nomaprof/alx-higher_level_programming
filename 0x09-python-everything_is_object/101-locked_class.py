48c82bfe4ae1cbeb4c218400c38229dd29fae00e
nomaprof <nomaprof@gmail.com>
2023-07-05 19:20:06 -0700

    Everything is object task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,4 @@
-#!/usr/python3
+#!/usr/bin/python3
 """
 Create a locked class now
 """
@@ -7,7 +7,3 @@ Create a locked class now
 class LockedClass():
     """the created locked class here"""
     __slots__ = ['first_name']
-
-    def __init__(self):
-        """initialize variables"""
-        pass
