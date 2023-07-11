7a42af1347e12815f01bf5e6c76492b0ed1c3259
nomaprof <nomaprof@gmail.com>
2023-07-10 20:32:36 -0700

    Python inheritance task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Tells the list of available attributes and methods of the object"""
+
+
+def lookup(obj):
+    """Tell us the attributes and methods of the object"""
+    return (dir(obj))
