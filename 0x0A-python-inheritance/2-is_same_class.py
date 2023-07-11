389ede99fdfe9ab524479e53f130cb6d95f96c76
nomaprof <nomaprof@gmail.com>
2023-07-11 05:03:40 -0700

    Python inheritance task

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,16 +3,7 @@
 """Checks if an object belongs to an instance of a certain class"""
 
 
-def is_same_class(obj, a_class)
-"""Confirm that an object is an instance of a given class
-    
-    Args:
-        obj (any): the instance
-        a_class (type): the class to match with object
-    Returns:
-        If object is an instance of the class - True.
-        contrarywise - False.
-    """
-        if not isinstance(a_class, type):
-        raise TypeError("a_class must be one type")
-    return (type(obj) is a_class)
+def is_same_class(obj, a_class):
+    """Confirm that an object is an instance of a given class
+    and return true or false"""
+    return (type(obj) == a_class)
