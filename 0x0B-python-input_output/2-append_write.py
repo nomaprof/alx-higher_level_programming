062e6b9335933323f2de4aec4ab577e2ccca62b3
nomaprof <nomaprof@gmail.com>
2023-07-12 20:45:47 -0700

    Python input and output task

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,6 +2,7 @@
 
 """Create a function for appending text to a file"""
 
+
 def append_write(filename="", text=""):
     """Append a text to the end of a file in UTF8 format"""
     with open(filename, "a", encoding="utf-8") as f:
