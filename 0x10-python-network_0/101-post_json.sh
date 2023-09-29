6f7b1506db1b070ecb7c3116f196c35f5b5f78a4
nomaprof <nomaprof@gmail.com>
2023-09-28 23:57:13 -0700

    Network o

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script passes a JSON filetype to a URL using the POST method and gets the body of the response
-curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"i
+curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
